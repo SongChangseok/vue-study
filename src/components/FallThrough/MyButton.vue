2734f9426793a1cfb235563355f4fb3d175350ac
cssong <a1373052011@gmail.com>
2023-09-26 21:42:34 +0900

    fall through 속성
    * 폴스루 속성을 제어하기 위해 defineOptions의 inheritAttrs를 false한다.
    * 그러면 루트 노드에 폴스루 속성이 자동으로 적용되지 않는다.
    * 그리고 를 통해 폴스루 속성을 다른 엘리먼트에 적용할 수 있다.
    * v-bind=''를 통해 폴스루 속성을 적용할 수 있다.

--- src/components/FallThrough/MyButton.vue
+++ src/components/FallThrough/MyButton.vue
@@ -1,7 +1,12 @@
 <script setup>
-const clickHandler = (e) => console.log('MyButton')
+defineOptions({
+  inheritAttrs: false // 속성 상속 비활성화, 루트 노드 이외에 다른 엘리먼트에 속성을 적용해야 하는 경우에 사용
+})
 </script>
 
 <template>
-  <button class="large" onclick="console.log('MyButton')">클릭하기</button>
+  <button class="large" onclick="console.log('MyButton')">
+    <span>풀스루 속성: {{ $attrs }}</span>
+  </button>
+  <button v-bind="$attrs">풀스루 속성을 이곳에</button>
 </template>
