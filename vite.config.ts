05e893cb100068ac045e357e0bc532fbcfad65c8
cssong <a1373052011@gmail.com>
2023-10-03 16:53:59 +0900

    tsconfig, vite.config 관련 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: '../dist'
+  },
+  server: {
+    port: 8080
   }
 })
